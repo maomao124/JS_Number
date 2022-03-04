3d626075db5daf47916042b577ff57785c0689e8
毛 <1296193245@qq.com>
2022-03-04 19:01:03 +0800

    JS_Number

---
+++ src/main/webapp/test1.js
@@ -0,0 +1,28 @@
+/**
+ * Project name(项目名称)：JS_Number
+ * File name(文件名): test1
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/3/4
+ * Time(创建时间)： 18:49
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+var x = 123;    // 整数
+var y = 3.14;   // 浮点数
+var z = 0xff;   // 十六进制数：255
+
+var x = 1.57e4;     // 等于 15700
+var y = 4.25e+6;    // 等于 4250000
+var z = 4.25e-6;    // 等于 0.00000425
+
+var a = 0o377;     // 等于 255
+var b = 0o123;     // 等于 83
+var c = 0xff;     // 等于 255
+var d = 0xb4;     // 等于 180
+
+
+var a = 5 / 0;      // 等于 Infinity
+var b = -5 / 0;     // 等于 -Infinity
---
+++ src/main/webapp/test3.js
@@ -0,0 +1,38 @@
+/**
+ * Project name(项目名称)：JS_Number
+ * File name(文件名): test3
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/3/4
+ * Time(创建时间)： 18:52
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+
+// 属性	    描述
+// Number.MAX_VALUE	JavaScript 中所能表示的最大值
+// Number.MIN_VALUE	JavaScript 中所能表示的最小值
+// Number.NaN	非数字
+// Number.NEGATIVE_INFINITY	负无穷，在溢出时返回
+// Number.POSITIVE_INFINITY	正无穷，在溢出时返回
+// Number.EPSILON	表示 1 与 Number 所能表示的大于 1 的最小浮点数之间的差
+// Number.MIN_SAFE_INTEGER	最小安全整数，即 -9007199254740991
+// Number.MAX_SAFE_INTEGER	最大安全整数，即 9007199254740991
+
+document.write("<br>");
+document.write(Number.MAX_VALUE);
+document.write("<br>");
+document.write(Number.MIN_VALUE);
+document.write("<br>");
+document.write(Number.NEGATIVE_INFINITY);
+document.write("<br>");
+document.write(Number.POSITIVE_INFINITY);
+document.write("<br>");
+document.write(Number.EPSILON);
+document.write("<br>");
+document.write(Number.MIN_SAFE_INTEGER);
+document.write("<br>");
+document.write(Number.MAX_SAFE_INTEGER);
+document.write("<br>");
